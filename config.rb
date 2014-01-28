5a0dd7a770068aa25eeee481e256a7e222c766dc
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-01-28 12:52:40 +0100

    Init

---
+++ config.rb
@@ -0,0 +1,8 @@
+# compass configuration
+# run with `compass compile` or `compass watch`
+http_path = "/"
+css_dir = "theme/css"
+sass_dir = "theme/scss"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :expanded
---
+++ slide_config.js
@@ -0,0 +1,20 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    // usePrettify: true, // Default: true
+    // enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    // enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/favicon.ico'
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jörn Zaeffererx',
+    company: '',
+    twitter: '@bassistance',
+    www: 'http://bassistance.de',
+    github: 'http://github.com/jzaefferer'
+  }]
+};
+
